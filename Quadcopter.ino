cc08b2a23acca3e502ace0405719764a5609de2e
Lukas <lukas_m@gmx.de>
2019-06-10 18:18:43 +0200

    Read all sensor values at ones.

--- Quadcopter.ino
+++ Quadcopter.ino
@@ -43,77 +43,77 @@ int32_t dt = 0;
 float dT = 0;
 
 // IMU raw measurements
-int16_t ax, ay, az;		// accelerometer
-int16_t gx, gy, gz;		// gyroscope
-int16_t hx, hy, hz;		// magnetometer
+int16_t ax, ay, az;     // accelerometer
+int16_t gx, gy, gz;     // gyroscope
+int16_t mx, my, mz;     // magnetometer
+int16_t temperature;    // temperature
 
-volatile bool imuInterrupt = false;		// indicates whether IMU interrupt pin has gone high
+volatile bool imuInterrupt = false;     // indicates whether IMU interrupt pin has gone high
 void imuReady() {
-	imuInterrupt = true;
+    imuInterrupt = true;
 }
 
 void setup() {
-	#ifdef DEBUG
-		// initialize serial communication
-		Serial.begin(115200);
-		while (!Serial);
-	#endif
+    #ifdef DEBUG
+        // initialize serial communication
+        Serial.begin(115200);
+        while (!Serial);
+    #endif
 
-	static int8_t imuStatus;
-	// start communication with IMU
-	imuStatus = IMU.init();
-	if (!imuStatus) {
-		DEBUG_PRINTLN("IMU initialization unsuccessful");
-		DEBUG_PRINTLN("Check IMU wiring or try cycling power");
-		while(1) {}
-	}
+    static int8_t imuStatus;
+    // start communication with IMU
+    imuStatus = IMU.init();
+    if (!imuStatus) {
+        DEBUG_PRINTLN("IMU initialization unsuccessful");
+        DEBUG_PRINTLN("Check IMU wiring or try cycling power");
+        while(1) {}
+    }
   
-	// Setup interrupt pin
-	pinMode(IMU_INTERRUPT_PIN, INPUT);
-	attachInterrupt(digitalPinToInterrupt(IMU_INTERRUPT_PIN), imuReady, FALLING);
+    // Setup interrupt pin
+    pinMode(IMU_INTERRUPT_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(IMU_INTERRUPT_PIN), imuReady, FALLING);
 }
 
 void loop() {
-	static uint32_t t0 = 0;
-	static uint32_t t = 0;
+    static uint32_t t0 = 0;
+    static uint32_t t = 0;
 
-	while (!imuInterrupt) {
-		// wait for the next interrupt
-	}
-	imuInterrupt = false;
-	
-	IMU.read_gyro(gx, gy, gz);
-	IMU.read_accel(ax, ay, az);
-	
-	t = micros();
-	dt = (t - t0);				// in us
-	dT = float(dt) * 0.000001;	// in s
+    while (!imuInterrupt) {
+        // wait for the next interrupt
+    }
+    imuInterrupt = false;
+    
+    IMU.read_sensors(ax, ay, az, gx, gy, gz, temperature, mx, my, mz);
+    
+    t = micros();
+    dt = (t - t0);              // in us
+    dT = float(dt) * 0.000001;  // in s
   
-	// update last IMU update time measurement
-	t0 = t;
-
-	// display the data
-	DEBUG_PRINTLN(dt);
-  
-	/*DEBUG_PRINT(dt);
-	DEBUG_PRINT("\t");
-	DEBUG_PRINT2(IMU.getAccelX_mss(),2);
-	DEBUG_PRINT("\t");
-	DEBUG_PRINT2(IMU.getAccelY_mss(),2);
-	DEBUG_PRINT("\t");
-	DEBUG_PRINT2(IMU.getAccelZ_mss(),2);
-	DEBUG_PRINT("\t");
-	DEBUG_PRINT2(IMU.getGyroX_rads(),2);
-	DEBUG_PRINT("\t");
-	DEBUG_PRINT2(IMU.getGyroY_rads(),2);
-	DEBUG_PRINT("\t");
-	DEBUG_PRINT2(IMU.getGyroZ_rads(),2);
-	DEBUG_PRINT("\t");
-	DEBUG_PRINT2(IMU.getMagX_uT(),2);
-	DEBUG_PRINT("\t");
-	DEBUG_PRINT2(IMU.getMagY_uT(),2);
-	DEBUG_PRINT("\t");
-	DEBUG_PRINT2(IMU.getMagZ_uT(),2);
-	DEBUG_PRINT("\t");
-	DEBUG_PRINTLN2(IMU.getTemperature_C(),2);*/
+    // update last IMU update time measurement
+    t0 = t;
+    
+    // display the data
+    DEBUG_PRINTLN(dt);
+    
+    /*DEBUG_PRINT(dt);
+    DEBUG_PRINT("\t");
+    DEBUG_PRINT2(IMU.getAccelX_mss(),2);
+    DEBUG_PRINT("\t");
+    DEBUG_PRINT2(IMU.getAccelY_mss(),2);
+    DEBUG_PRINT("\t");
+    DEBUG_PRINT2(IMU.getAccelZ_mss(),2);
+    DEBUG_PRINT("\t");
+    DEBUG_PRINT2(IMU.getGyroX_rads(),2);
+    DEBUG_PRINT("\t");
+    DEBUG_PRINT2(IMU.getGyroY_rads(),2);
+    DEBUG_PRINT("\t");
+    DEBUG_PRINT2(IMU.getGyroZ_rads(),2);
+    DEBUG_PRINT("\t");
+    DEBUG_PRINT2(IMU.getMagX_uT(),2);
+    DEBUG_PRINT("\t");
+    DEBUG_PRINT2(IMU.getMagY_uT(),2);
+    DEBUG_PRINT("\t");
+    DEBUG_PRINT2(IMU.getMagZ_uT(),2);
+    DEBUG_PRINT("\t");
+    DEBUG_PRINTLN2(IMU.getTemperature_C(),2);*/
 }
