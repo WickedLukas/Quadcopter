23d6197fbad9b8705af9df4ba3666b53556d6c83
lukas_m@gmx.de <lukas_m@gmx.de>
2019-08-21 21:37:30 +0200

    Use high beta value until Madgwick filter converged.

--- Quadcopter.ino
+++ Quadcopter.ino
@@ -1,9 +1,9 @@
 /*
 * Quadcopter.ino
 *
-* Created: 26.04.2019
-* Author: Lukas
-* 
+* Created:	26.04.2019
+* Author:	Lukas
+*
 */
 #include <Arduino.h>
 
@@ -19,12 +19,12 @@ void calc_accelAngles(float& angle_x_accel, float& angle_y_accel);
 // Debug output is now working even on ATMega328P MCUs (e.g. Arduino Uno)
 // after moving string constants to flash memory storage using the F()
 // compiler macro (Arduino IDE 1.0+ required).
-#define DEBUG
+//#define DEBUG
 
-// define if you want to send imu data through serial (for example to visualize it in "Processing")
-//#define SEND_SERIAL
+// send imu data through serial (for example to visualize it in "Processing")
+#define SEND_SERIAL
 
-// define if you want to calibrate the imu
+// calibrate the imu
 //#define IMU_CALIBRATION
 
 #ifdef DEBUG
@@ -44,10 +44,12 @@ void calc_accelAngles(float& angle_x_accel, float& angle_y_accel);
 #define IMU_CS_PIN 10
 #define IMU_INTERRUPT_PIN 24
 
-#define BETA_INIT 50	// Madgwick algorithm gain (2 * proportional gain (Kp)) during initial pose estimation
+#define BETA_INIT 10	// Madgwick algorithm gain (2 * proportional gain (Kp)) during initial pose estimation
 #define BETA 0.041		// Madgwick algorithm gain (2 * proportional gain (Kp))
 
-#define INIT_ANGLE_DIFF -1  // Maximum angle difference between the accelerometer angle and the filtered angle after initialization
+// parameters to check if filtered angles converged during initialization
+#define INIT_ANGLE_DIFFERENCE 0.5	// Maximum angle difference between filtered angles and accelerometer angles (x- and y-axis) after initialization
+#define INIT_ANGULAR_VELOCITY 0.05	// Maximum angular velocity of filtered angle (z-axis) after initialization
 
 // object for ICM-20948 imu
 ICM20948_SPI imu(IMU_CS_PIN, IMU_SPI_PORT);
@@ -77,6 +79,9 @@ void imuReady() {
 }
 
 void setup() {
+	// last quadcopter z-axis angle
+	float angle_z_last = 0;
+	
 	#ifdef DEBUG
 		// initialize serial communication
 		Serial.begin(115200);
@@ -134,19 +139,22 @@ void setup() {
 		
 		madgwickFilter.get_euler(angle_x, angle_y, angle_z, dt_s, ax, ay, az, gx_rps, gy_rps, gz_rps, mx, my, mz);
 		
-		//madgwickFilter.set_angle_z(0);
-		
 		calc_accelAngles(angle_x_accel, angle_y_accel);
 		
-		if ((abs(angle_x_accel - angle_x) < INIT_ANGLE_DIFF) && (abs(angle_y_accel - angle_y) < INIT_ANGLE_DIFF)) {
+		// initialization is completed if filtered angles converged
+		if ((abs(angle_x_accel - angle_x) < INIT_ANGLE_DIFFERENCE) && (abs(angle_y_accel - angle_y) < INIT_ANGLE_DIFFERENCE)
+		&& ((abs(angle_z - angle_z_last) / dt_s) < INIT_ANGULAR_VELOCITY)) {
+			// reduce beta value, since filtered angles have stabilized during initialization
 			madgwickFilter.set_beta(BETA);
 			
 			init = false;
 			
 			DEBUG_PRINTLN(F("Initial pose estimated."));
-			DEBUG_PRINTLN(abs(angle_x_accel - angle_x));
-			DEBUG_PRINTLN(abs(angle_y_accel - angle_y));
+			DEBUG_PRINTLN2(abs(angle_x_accel - angle_x), 6);
+			DEBUG_PRINTLN2(abs(angle_y_accel - angle_y), 6);
+			DEBUG_PRINTLN2(abs(angle_z - angle_z_last) / dt_s, 6);
 		}
+		angle_z_last = angle_z;
 		
 		// run serial print at a rate independent of the main loop
 		static uint32_t t0_serial = micros();
@@ -182,7 +190,7 @@ void loop() {
 	
 	
 	
-	// run serial print at a rate independent of the main loop
+	// run serial print at a rate independent of the main loop (t0_serial = 16666 for 60 Hz update rate)
 	static uint32_t t0_serial = micros();
 	if (micros() - t0_serial > 16666) {
 		t0_serial = micros();
@@ -193,39 +201,10 @@ void loop() {
 		
 		static float angle_x_accel, angle_y_accel;
 		calc_accelAngles(angle_x_accel, angle_y_accel);
-		DEBUG_PRINT(angle_x_accel); DEBUG_PRINT("\t"); DEBUG_PRINT(angle_y_accel); DEBUG_PRINT("\t"); DEBUG_PRINTLN(0);
-		DEBUG_PRINT(angle_x); DEBUG_PRINT("\t"); DEBUG_PRINT(angle_y); DEBUG_PRINT("\t"); DEBUG_PRINTLN(angle_z);
+		//DEBUG_PRINT(angle_x_accel); DEBUG_PRINT("\t"); DEBUG_PRINT(angle_y_accel); DEBUG_PRINT("\t"); DEBUG_PRINTLN(0);
+		//DEBUG_PRINT(angle_x); DEBUG_PRINT("\t"); DEBUG_PRINT(angle_y); DEBUG_PRINT("\t"); DEBUG_PRINTLN(angle_z);
 		
-		//DEBUG_PRINT(ax); DEBUG_PRINT("\t"); DEBUG_PRINT(ay); DEBUG_PRINT("\t"); DEBUG_PRINTLN(az);
-		//DEBUG_PRINT(gx_rps); DEBUG_PRINT("\t"); DEBUG_PRINT(gy_rps); DEBUG_PRINT("\t"); DEBUG_PRINTLN(gz_rps);
-		//DEBUG_PRINT(mx); DEBUG_PRINT("\t"); DEBUG_PRINT(my); DEBUG_PRINT("\t"); DEBUG_PRINTLN(mz);
-		
-		DEBUG_PRINTLN();
-		
-		//DEBUG_PRINT(ax_g); DEBUG_PRINT("\t"); DEBUG_PRINT(ay_g); DEBUG_PRINT("\t"); DEBUG_PRINTLN(az_g);
-		//DEBUG_PRINT(mx_ut); DEBUG_PRINT("\t"); DEBUG_PRINT(my_ut); DEBUG_PRINT("\t"); DEBUG_PRINTLN(mz_ut);
-		
-		/*DEBUG_PRINT(dt);
-		DEBUG_PRINT("\t");
-		DEBUG_PRINT2(imu.getAccelX_mss(),2);
-		DEBUG_PRINT("\t");
-		DEBUG_PRINT2(imu.getAccelY_mss(),2);
-		DEBUG_PRINT("\t");
-		DEBUG_PRINT2(imu.getAccelZ_mss(),2);
-		DEBUG_PRINT("\t");
-		DEBUG_PRINT2(imu.getGyroX_rads(),2);
-		DEBUG_PRINT("\t");
-		DEBUG_PRINT2(imu.getGyroY_rads(),2);
-		DEBUG_PRINT("\t");
-		DEBUG_PRINT2(imu.getGyroZ_rads(),2);
-		DEBUG_PRINT("\t");
-		DEBUG_PRINT2(imu.getMagX_uT(),2);
-		DEBUG_PRINT("\t");
-		DEBUG_PRINT2(imu.getMagY_uT(),2);
-		DEBUG_PRINT("\t");
-		DEBUG_PRINT2(imu.getMagZ_uT(),2);
-		DEBUG_PRINT("\t");
-		DEBUG_PRINTLN2(imu.getTemperature_C(),2);*/
+		//DEBUG_PRINTLN();
 		
 		#ifdef SEND_SERIAL
 			// Send data to "Processing" for visualization
